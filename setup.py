d5d7e274be6bb8e509e24da4ae4bbddfd832e52b
Rashad <pythonaz@yahoo.com>
2017-05-02 15:56:14 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+name='mediaf',
+py_modules=['pyardu'],
+version='1',
+description='pip install mediaf',
+url='https://github.com/RashadGarayev/mediaf',
+author='Rashad Garayev',
+author_email='pythonaz@yahoo.com',
+license='MIT'
+)
