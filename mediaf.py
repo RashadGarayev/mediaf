3f7f583092fed2575f70fd1b6d7f7b84694584a5
Rashad <pythonaz@yahoo.com>
2017-05-02 23:01:05 +0300

    Update mediaf.py

--- mediaf.py
+++ mediaf.py
@@ -2,9 +2,9 @@ import os
 import getpass,string,random
 
 def version():
-	print "_Version_1"
+	print ("_Version_1")
 def about():
-	print"""
+	print("""
 	Usage --------------------------------------
 	First install sudo apt-get install ffmpeg
 	--------------------------------------------
@@ -56,7 +56,7 @@ def about():
 
 
 
-	"""
+	""")
 def play(infile):
 	os.system("ffplay {}".format(infile))
 def convert_avi(infile):
@@ -66,11 +66,11 @@ def convert_avi(infile):
 		outputname=random.sample(v,4)
 		creat="".join(outputname)
 		os.system("ffmpeg -i {} -acodec copy -vcodec copy '/home/{}/Videos/{}.avi'".format(infile,i,str(creat)))
-		print "file created in /home/{}/Videos/{}.avi".format(i,creat)
+		print ("file created in /home/{}/Videos/{}.avi".format(i,creat))
 	except IOError:
-		print"[-] file format not supported"
+		print("[-] file format not supported")
 	except:
-		print"[-] Sorry some problem"
+		print("[-] Sorry some problem")
 def convert_mp4(infile):
 	try:
 		i=getpass.getuser()
@@ -78,11 +78,11 @@ def convert_mp4(infile):
 		outputname=random.sample(v,5)
 		creat="".join(outputname)
 		os.system("ffmpeg -i {}  -acodec aac -strict experimental -ac 2 -ab 160k -vcodec libx264 -preset slow -f mp4 -crf 22 '/home/{}/Videos/{}.mp4'".format(infile,i,str(creat)))
-		print "file created in /home/{}/Videos/{}.avi".format(i,creat)
+		print ("file created in /home/{}/Videos/{}.avi".format(i,creat))
 	except IOError:
-		print"[-] file format not supported"
+		print("[-] file format not supported")
 	except:
-		print"[-] Sorry some problem"
+		print("[-] Sorry some problem")
 def convert_vob(infile):
 	try:
 		i=getpass.getuser()
@@ -90,11 +90,11 @@ def convert_vob(infile):
 		outputname=random.sample(v,5)
 		creat="".join(outputname)
 		os.system("ffmpeg -i {}  -target ntsc-dvd -preset ultrafast '/home/{}/Videos/{}.vob'".format(infile,i,str(creat)))
-		print "file created in /home/{}/Videos/{}.avi".format(i,creat)
+		print ("file created in /home/{}/Videos/{}.avi".format(i,creat))
 	except IOError:
-		print"[-] file format not supported"
+		print("[-] file format not supported")
 	except:
-		print"[-] Sorry some problem"
+		print("[-] Sorry some problem")
 
 
 def convert_flv(infile):
@@ -104,11 +104,11 @@ def convert_flv(infile):
 		outputname=random.sample(v,5)
 		creat="".join(outputname)
 		os.system("ffmpeg -i {} -vcodec flv -b:v 512k -s 480x360 -r 30 -acodec libmp3lame -ar 44100 -f flv '/home/{}/Videos/{}.flv'".format(infile,i,str(creat)))
-		print "file created in /home/{}/Videos/{}.avi".format(i,creat)
+		print ("file created in /home/{}/Videos/{}.avi".format(i,creat))
 	except IOError:
-		print"[-] file format not supported"
+		print("[-] file format not supported")
 	except:
-		print"[-] Sorry some problem"
+		print("[-] Sorry some problem")
 def convert_dv(infile):
 	try:
 		i=getpass.getuser()
@@ -116,11 +116,11 @@ def convert_dv(infile):
 		outputname=random.sample(v,5)
 		creat="".join(outputname)
 		os.system("ffmpeg -i {} -s pal -r pal -aspect 4:3 -ar 48000 -ac 2 '/home/{}/Videos/{}.dv'".format(infile,i,str(creat)))
-		print "file created in /home/{}/Videos/{}.avi".format(i,creat)
+		print ("file created in /home/{}/Videos/{}.avi".format(i,creat))
 	except IOError:
-		print"[-] file format not supported"
+		print("[-] file format not supported")
 	except:
-		print"[-] Sorry some problem"
+		print("[-] Sorry some problem")
 def convert_mkv(infile):
 	try:
 		i=getpass.getuser()
@@ -128,11 +128,11 @@ def convert_mkv(infile):
 		outputname=random.sample(v,5)
 		creat="".join(outputname)
 		os.system("ffmpeg -i {} -f matroska -vcodec libx264 -acodec libvorbis '/home/{}/Videos/{}.mkv'".format(infile,i,str(creat)))
-		print "file created in /home/{}/Videos/{}.avi".format(i,creat)
+		print ("file created in /home/{}/Videos/{}.avi".format(i,creat))
 	except IOError:
-		print"[-] file format not supported"
+		print("[-] file format not supported")
 	except:
-		print"[-] Sorry some problem"
+		print("[-] Sorry some problem")
 def convert_mpeg(infile):
 	try:
 		i=getpass.getuser()
@@ -140,11 +140,11 @@ def convert_mpeg(infile):
 		outputname=random.sample(v,5)
 		creat="".join(outputname)
 		os.system("ffmpeg -i {} -target ntsc-dvd '/home/{}/Videos/{}.mpeg'".format(infile,i,str(creat)))
-		print "file created in /home/{}/Videos/{}.avi".format(i,creat)
+		print ("file created in /home/{}/Videos/{}.avi".format(i,creat))
 	except IOError:
-		print"[-] file format not supported"
+		print("[-] file format not supported")
 	except:
-		print"[-] Sorry some problem"
+		print("[-] Sorry some problem")
 def convert_mov(infile):
 	try:
 		i=getpass.getuser()
@@ -152,11 +152,11 @@ def convert_mov(infile):
 		outputname=random.sample(v,5)
 		creat="".join(outputname)
 		os.system("ffmpeg -i {} -acodec libmp3lame -ab 192 '/home/{}/Videos/{}.mov'".format(infile,i,str(creat)))
-		print "file created in /home/{}/Videos/{}.avi".format(i,creat)
+		print ("file created in /home/{}/Videos/{}.avi".format(i,creat))
 	except IOError:
-		print"[-] file format not supported"
+		print("[-] file format not supported")
 	except:
-		print"[-] Sorry some problem"
+		print("[-] Sorry some problem")
 def convert_webm(infile):
 	try:
 		i=getpass.getuser()
@@ -164,11 +164,11 @@ def convert_webm(infile):
 		outputname=random.sample(v,5)
 		creat="".join(outputname)
 		os.system("ffmpeg -i {} -cpu-used 4 -threads 8 '/home/{}/Videos/{}.webm'".format(infile,i,str(creat)))
-		print "file created in /home/{}/Videos/{}.avi".format(i,creat)
+		print ("file created in /home/{}/Videos/{}.avi".format(i,creat))
 	except IOError:
-		print"[-] file format not supported"
+		print("[-] file format not supported")
 	except:
-		print"[-] Sorry some problem"
+		print("[-] Sorry some problem")
 def convert_wmv(infile):
 	try:
 		i=getpass.getuser()
@@ -176,11 +176,11 @@ def convert_wmv(infile):
 		outputname=random.sample(v,5)
 		creat="".join(outputname)
 		os.system("ffmpeg -i {} -b:v 2M -vcodec msmpeg4 -acodec wmav2 '/home/{}/Videos/{}.wmv'".format(infile,i,str(creat)))
-		print "file created in /home/{}/Videos/{}.avi".format(i,creat)
+		print( "file created in /home/{}/Videos/{}.avi".format(i,creat))
 	except IOError:
-		print"[-] file format not supported"
+		print("[-] file format not supported")
 	except:
-		print"[-] Sorry some problem"
+		print("[-] Sorry some problem")
 def convert_3gp(infile):
 	try:
 		i=getpass.getuser()
@@ -188,11 +188,11 @@ def convert_3gp(infile):
 		outputname=random.sample(v,5)
 		creat="".join(outputname)
 		os.system("ffmpeg -i {} -f 3gp -vcodec h263 -acodec amr_nb '/home/{}/Videos/{}.3gp'".format(infile,i,str(creat)))
-		print "file created in /home/{}/Videos/{}.avi".format(i,creat)
+		print ("file created in /home/{}/Videos/{}.avi".format(i,creat))
 	except IOError:
-		print"[-] file format not supported"
+		print("[-] file format not supported")
 	except:
-		print"[-] Sorry some problem"
+		print("[-] Sorry some problem")
 def getaudio(infile):
 	try:
 		i=getpass.getuser()
@@ -200,11 +200,11 @@ def getaudio(infile):
 		outputname=random.sample(v,5)
 		creat="".join(outputname)
 		os.system("ffmpeg -i {} -f mp3 '/home/{}/Videos/{}.mp3'".format(infile,i,str(creat)))
-		print "file created in /home/{}/Videos/{}.avi".format(i,creat)
+		print ("file created in /home/{}/Videos/{}.avi".format(i,creat))
 	except IOError:
-		print"[-] file format not supported"
+		print("[-] file format not supported")
 	except:
-		print"[-] Sorry some problem"
+		print("[-] Sorry some problem")
 
 
 		
