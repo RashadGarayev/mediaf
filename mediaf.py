c083dacb57b123a9ae1a41119cb6a0450fc3b3c6
Rashad <pythonaz@yahoo.com>
2017-05-02 16:27:57 +0300

    Update mediaf.py

--- mediaf.py
+++ mediaf.py
@@ -1,2 +1,21 @@
 import os
-os.system("ffplay")
+def version():
+	print "_Version_1"
+def about():
+	print"""
+	Usage --------------------------------------
+	First install sudo apt-get ffmpeg
+	--------------------------------------------
+	>>import mediaf
+	>>from mediaf import*
+	>>mediaf.version()
+	_Version_1
+	>>mediaf.play("input.mp4")
+	"""
+def play(infile):
+	try:
+		os.system("ffplay '{}'".format(str(infile)))
+	except SyntaxError:
+		print "invalid systax\n {}".format(infile)
+		print "Use file in string arg exam : mediaf.play('/home/user/input.mp4')"
+
